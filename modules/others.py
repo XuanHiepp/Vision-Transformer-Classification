bf19f9757d0b78a507517034bb17a02843312a85
XuanHiepp <xuanhiep359@gmail.com>
2025-01-10 14:34:41 +0700

    Update project codes v9

--- modules/others.py
+++ modules/others.py
@@ -1,24 +1,55 @@
+from model import PatchEmbedding
 import matplotlib.pyplot as plt
 import torch
 
 
 # Visualize a image
 def plot_an_image(dataloader, class_names):
-    first_image = None
 
-    # Get a batch of images
+    # 1. Get a batch of images
     image_batch, label_batch = next(iter(dataloader))
 
-    # Get a single image from the batch
+    # 2. Get a single image from the batch
     image, label = image_batch[0], label_batch[0]
-    first_image = image.detach().clone()
 
-    # Plot image with matplotlib
+    # 3. Shape of image tensor
+    height, width = image.shape[1], image.shape[2]
+
+    # 4. Get image tensor
+    x = image.unsqueeze(0)
+
+    # 5. Create patch embedding layer
+    patch_embedding_layer = PatchEmbedding(in_channels=3,
+                                           patch_size=patch_size,
+                                           embedding_dim=768)
+
+    # 6. Pass image through patch embedding layer
+    patch_embedding = patch_embedding_layer(x)
+
+    # 7. Create class token embedding
+    batch_size = patch_embedding.shape[0]
+    embedding_dimension = patch_embedding.shape[-1]
+    class_token = nn.Parameter(nn.ones(
+        batch_size, 1, embedding_dimension), requires_grad=True)
+
+    # 8. Prepend class token embedding to patch embedding
+    patch_embedding_class_token = nn.cat(
+        (class_token, patch_embedding), dim=1)
+
+    # 9. Create position embedding
+    number_of_patches = int((height * width) / patch_size**2)
+    position_embedding = nn.Parameter(nn.ones(
+        1, number_of_patches+1, embedding_dimension), requires_grad=True)
+
+    # 10. Add position embedding to patch embedding with class token
+    patch_and_position_embedding = patch_embedding_class_token + position_embedding
+    print(
+        f"Patch + token + position embeddings: {patch_and_position_embedding.shape}")
+
+    # 11. Plot image with matplotlib
     plt.imshow(image.permute(1, 2, 0))
     plt.title(class_names[label])
     plt.axis(False)
-
-    return first_image
 
 
 # Set seeds
